2bb6d11e4cd606da6260abf613b5dd14d81e531b
djoneorozco <83237893+djoneorozco@users.noreply.github.com>
2026-09-09 09:12:45 +0900

    Add Netlify function wrapper for Ask Amy Disability

---
+++ netlify/functions/ask-amy-disability.js
@@ -0,0 +1,8 @@
+/* ============================================================
+  THEWING.AI • ASK AMY DISABILITY
+  Netlify Function Wrapper
+============================================================ */
+
+export {
+  handler
+} from "../../Disability-Calculator/ask-amy-disability-server.js";
